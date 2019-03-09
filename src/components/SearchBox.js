c1099e27660cb63f560891ce7c42f0eacfc13eaa
Hasina Akhter <hakhter2002@yahoo.com>
2019-03-09 10:01:28 -0500

    Update SearchBox.js

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchBox = ({searchfield, searchChange} ) =>{
   return(
      <div>
-     <input className="pa2 ba b--green bg-lightest-blue"
+     <input className="pa3 ba b--green bg-lightest-blue"
      type="search" 
      placeholder="search robots" 
      onChange= {searchChange}
