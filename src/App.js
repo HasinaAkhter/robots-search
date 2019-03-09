2b5876dbf9cf64f736c295107ebdadb25848ad9e
Hasina Akhter <hakhter2002@yahoo.com>
2019-03-09 09:58:02 -0500

    Update App.js
    the app is updated: we are adding uses through API  for which we use react's life cycle hook. componentDidMount()

--- src/App.js
+++ src/App.js
@@ -9,10 +9,18 @@ class App extends React.Component{
   constructor(){
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
+  
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then(users => {this.setState({ robots: users})});
+    
+  }
+  
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value })
   }
